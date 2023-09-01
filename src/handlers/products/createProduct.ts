4510d3c6b5ce9558415ecd5541e369ad9ae90676
Bishalsarang <bishalsarang@gmail.com>
2023-09-01 22:18:42 +0545

    Update dynamodb client and integrate create product

--- src/handlers/products/createProduct.ts
+++ src/handlers/products/createProduct.ts
@@ -1,15 +1,23 @@
-import { APIGatewayEvent } from 'aws-lambda';
+import { APIGatewayProxyResult } from 'aws-lambda';
+import dynamoDbDocumentClient from '../../services/dynamoDbDocumentClient';
 
-export const handler = async (event: APIGatewayEvent) => {
+import * as process from 'process';
+
+export const handler = async (): Promise<APIGatewayProxyResult> => {
+  const item = {
+    id: '1',
+    name: 'Asus Zephyrus',
+  };
+
+  await dynamoDbDocumentClient
+    .put({
+      Item: item,
+      TableName: process.env.PRODUCTS_TABLE_NAME,
+    })
+    .promise();
+
   return {
     statusCode: 201,
-    body: JSON.stringify(
-      {
-        message: 'Created a product successfully!',
-        input: event,
-      },
-      null,
-      2,
-    ),
+    body: JSON.stringify(item),
   };
 };
---
+++ src/services/dynamoDbDocumentClient.ts
@@ -0,0 +1,17 @@
+import * as AWS from 'aws-sdk';
+import * as process from 'process';
+
+import { config } from 'dotenv';
+
+config();
+
+let dynamoDbDocumentClient: AWS.DynamoDB.DocumentClient;
+dynamoDbDocumentClient = new AWS.DynamoDB.DocumentClient();
+
+if (process.env.ENVIRONMENT === 'local') {
+  dynamoDbDocumentClient = new AWS.DynamoDB.DocumentClient({
+    endpoint: 'http://localhost:7000',
+  });
+}
+
+export default dynamoDbDocumentClient;

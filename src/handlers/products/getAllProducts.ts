dfe32a47108501a26356b48f64ac35476df291c6
Bishalsarang <bishalsarang@gmail.com>
2023-09-01 23:21:12 +0545

    Support fetching all products

--- src/handlers/products/getAllProducts.ts
+++ src/handlers/products/getAllProducts.ts
@@ -1,15 +1,28 @@
-import { APIGatewayEvent } from 'aws-lambda';
+import { AttributeMap, DocumentClient } from 'aws-sdk/clients/dynamodb';
 
-export const handler = async (event: APIGatewayEvent) => {
+import dynamoDbDocumentClient from '../../services/dynamoDbDocumentClient';
+
+export const handler = async () => {
+  let items: AttributeMap[] = [];
+  let lastEvaluatedKey: DocumentClient.Key | undefined;
+
+  do {
+    const data = await dynamoDbDocumentClient
+      .scan({
+        TableName: process.env.PRODUCTS_TABLE_NAME,
+        ExclusiveStartKey: lastEvaluatedKey,
+      })
+      .promise();
+
+    if (data.Items && data.Items.length > 0) {
+      items = [...items, ...data.Items];
+    }
+
+    lastEvaluatedKey = data.LastEvaluatedKey;
+  } while (lastEvaluatedKey != null);
+
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Get all products successfully!',
-        input: event,
-      },
-      null,
-      2,
-    ),
+    body: JSON.stringify(items),
   };
 };

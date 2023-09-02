b28c4f0f454f21364b4198c5581aed1a3809f662
Bishalsarang <bishalsarang@gmail.com>
2023-09-02 10:08:06 +0545

    Add test endpoint.

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,5 +1,13 @@
 import { handler as createProduct } from './products/createProduct';
 import { handler as deleteProduct } from './products/deleteProduct';
 import { handler as getAllProducts } from './products/getAllProducts';
+import { APIGatewayProxyResult } from 'aws-lambda';
 
-export { createProduct, deleteProduct, getAllProducts };
+async function hello(): Promise<APIGatewayProxyResult> {
+  return {
+    statusCode: 200,
+    body: JSON.stringify('Hello World!'),
+  };
+}
+
+export { createProduct, deleteProduct, getAllProducts, hello };

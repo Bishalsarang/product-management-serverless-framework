318c18e1eda6ac9c72d749cb4b6397e089a6c8b5
Bishalsarang <bishalsarang@gmail.com>
2023-09-06 16:17:31 +0545

    Add bucket to store photos

--- src/module.d.ts
+++ src/module.d.ts
@@ -2,6 +2,8 @@ import { Env } from './types';
 
 declare global {
   namespace NodeJS {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
     interface ProcessEnv extends Env {}
   }
 }

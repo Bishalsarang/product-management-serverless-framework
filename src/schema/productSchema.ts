d61ea0625ba3f63b64dd2fbe4fbb68ecb7187e93
Bishalsarang <bishalsarang@gmail.com>
2023-09-07 17:22:33 +0545

    Fix schema validation message

--- src/schema/productSchema.ts
+++ src/schema/productSchema.ts
@@ -1,10 +1,17 @@
 import { z } from 'zod';
 
 export const createProductRequestSchema = z.object({
-  name: z.string().min(3).max(255),
-  price: z.number().positive(),
+  name: z
+    .string()
+    .min(3, 'Name must be at least 3 characters long')
+    .max(255, 'Name cannot exceed 255 characters'),
+  price: z.number().positive('Price must be a positive number'),
   description: z.string().optional(),
-  imageURL: z.string().url().optional().or(z.literal('')),
+  imageURL: z
+    .string()
+    .url('Image URL must be a valid URL')
+    .optional()
+    .or(z.literal('')),
 });
 
 export const productSchema = createProductRequestSchema.extend({
